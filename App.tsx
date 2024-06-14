8ccb4a32e8a612044197053cf8313ffb0c1a1732
Pazhani <pazhanivel11111@gmail.com>
2024-06-14 16:13:09 +0530

    c

--- App.tsx
+++ App.tsx
@@ -1,11 +1,5 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React, {useState} from 'react';
+import LoginScreen from './src/LoginScreen';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -17,39 +11,17 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-// import Icon from 'react-native-vector-icons/MaterialIcons';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
-  const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <View style={styles.mainView}>
-      <View style={styles.secondView}>
-        <View style={styles.thirdView}>
-          <Text style={styles.tick}>✓</Text>
-        </View>
-        <Text style={styles.firstText}>Success</Text>
-        <Text style={styles.secondText}>
-          Your message was sent successfully,{'\n'} please check your mail to
-          confirm
-        </Text>
-        <Pressable style={styles.buttonStyling}>
-          <Text style={styles.buttonText}>Continue</Text>
-        </Pressable>
-      </View>
-    </View>
+<View style={customStyles.mainView}>
+<LoginScreen loginTitle="Hello"/>
+</View>
   );
 }
 
-const styles = StyleSheet.create({
+const customStyles = StyleSheet.create({
   mainView: {
     width: '100%',
     height: '100%',
@@ -57,54 +29,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  secondView: {
-    width: '90%',
-    height: '98%',
-    borderRadius: 30,
-    backgroundColor: 'white',
-    alignItems: 'center',
-  },
-  thirdView: {
-    width: 186,
-    height: 186,
-    backgroundColor: '#FFE5B4',
-    borderRadius: 93,
-    marginTop: 77,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  tick: {
-    color: '#FF4500',
-    fontSize: 95,
-  },
-  firstText: {
-    marginTop: 51,
-    fontSize: 40,
-    fontWeight: 'bold',
-    color: 'black',
-  },
-  secondText: {
-    marginTop: 17,
-    marginLeft: 30,
-    marginRight: 30,
-    color: 'gray',
-    fontSize: 17,
-    fontWeight: 'bold',
-  },
-  buttonStyling: {
-    backgroundColor: '#FF4500',
-    width: '83%',
-    padding: 15,
-    borderRadius: 10,
-    marginTop: 300,
-    alignItems: 'center',
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
 });
+
 
 export default App;
